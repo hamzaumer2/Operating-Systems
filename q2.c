1657f5b2cdd97f5b089bfb7b17193a63d20a91e7
Hamza Umer <71342062+hamzaumer2@users.noreply.github.com>
2022-06-18 15:57:35 +0500

    Add files via upload

---
+++ q2.c
@@ -0,0 +1,58 @@
+//HAMZA UMER FAROOQ
+//200789 - BSCS-4-B
+//OS LAB 9
+//QUESTION 2
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
+
+typedef struct thread_data {
+   int a, b, sum;
+
+} thread_data;
+
+
+void *t1(void *arg)
+{
+   printf("\nINSIDE THREAD, RETURNING SUM TO PARENT");
+
+   thread_data *tdata=(thread_data *)arg;
+
+   int a=tdata->a, b=tdata->b, sum=a+b;
+
+   tdata->sum=sum;
+   pthread_exit(NULL);
+}
+
+
+
+
+int main()
+{
+   pthread_t tid;
+   thread_data tdata;
+   int first, second;
+
+
+printf("\nENTER FIRST NUMBER: ");
+scanf("%d", &first);
+
+printf("\nENTER SECOND NUMBER: ");
+scanf("%d", &second);
+
+   tdata.a=first, tdata.b=second;
+   printf("\nBEFORE PASSING DATA TO THREAD");
+
+
+   pthread_create(&tid, NULL, t1, (void *)&tdata);
+   pthread_join(tid, NULL);
+
+
+   printf("\nBACK IN PARENT TO PRINT SUM: ");
+   printf("%d + %d = %d\n", tdata.a, tdata.b, tdata.sum);
+
+   return 0;
+}
