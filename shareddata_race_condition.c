07ff13b360f3dfd6014dab4576a698d69cdde679
Hamza Umer <71342062+hamzaumer2@users.noreply.github.com>
2022-07-02 21:56:47 +0500

    Add files via upload

---
+++ shareddata_race_condition.c
@@ -0,0 +1,27 @@
+/*
+
+*  shareddata.c:
+*  compile: $ gcc shareddata.c -lpthread
+*  usage: $./a.out 
+*/
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+char** ptr;
+void * thread_function(void * localarg);
+int main(){
+   pthread_t tid[2];
+   char* msg[2] = {"Hello from Ali", "Hello from Raju"};
+   ptr = msg;
+   for(int i=0;i<2;i++){
+      pthread_create(&tid[i], NULL, thread_function, (void*)&i);
+      pthread_join(tid[i], NULL);
+   }
+   return 0;
+}
+void * thread_function(void * localarg){
+   int myid = *((int*)localarg);
+   static int svar = 0;
+   printf("[%d]: %s (svar = %d)\n", myid, ptr[myid], ++svar);
+   pthread_exit(NULL);
+}
